5e3cc766a383a629ec9ef85bb2ac962e1aa224aa
admin <aneethunair@gmail.com>
2021-06-28 11:17:30 +0530

    new file

---
+++ app/src/main/java/com/efficientindia/securescan/ui/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.efficientindia.securescan.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.efficientindia.securescan.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
